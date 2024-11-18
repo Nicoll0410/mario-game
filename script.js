853ec4aa9a5e3cd6b48ce34c2f131b2c06da8184
Nicoll0410 <nicollgiraldo0410@gmail.com>
2024-11-18 17:36:07 -0500

    se agregan las imagenes para el proyecto

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import platform from './img/platform.png'
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
@@ -27,6 +28,7 @@ class Player {
 
     update() {
         this.draw()
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
         if (this.position.y + this.height + this.velocity.y <= canvas.height)
@@ -35,12 +37,116 @@ class Player {
     }
 }
 
-const player = new Player()
+class Platform {
+    constructor({ x, y }) {
+        this.position = {
+            x,
+            y
+        }
+        this.width = 200
+        this.height = 20
+    }
 
+    draw() {
+        c.fillStyle = "blue"
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const player = new Player()
+const platform = new Platform()
+const platforms = [new Platform({x : 200, y : 100}) , new Platform({ x : 500, y : 200})]
+
+const keys = {
+    right : {
+        pressed : false
+    },
+    left : {
+        pressed : false
+    }
+}
+
+let scrollOfset = 0
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
+    platforms.forEach(platform => {
+        platform.draw()
+    })
+
+    if (keys.right.pressed && player.position.x < 400) {
+        player.velocity.x = 5
+    } else if (keys.left.pressed && player.position.x > 100) {
+        player.velocity.x = -5
+    } else {
+        player.velocity.x = 0
+        if (keys.right.pressed) {
+            scrollOfset += 5
+            platforms.forEach(platform => {
+                platform.position.x -= 5
+            })
+        } else if (keys.left.pressed) {
+            scrollOfset -= 5
+            platforms.forEach(platform => {
+                platform.position.x += 5
+            })
+        }
+    }
+    //detección de colisión de plataforma
+    platforms.forEach(platform => {
+        platform.draw()
+        if (player.position.y + player.height <= platform.position.y && player.position.y + player.height
+            + player.velocity.y >= platform.position.y && player.position.x + player.width
+            >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
+                player.velocity.y = 0
+            }
+    })
+
+    if (scrollOfset > 2000) {
+        
+    }
 }
 
 animate()
+
+addEventListener('keydown' , ({ keyCode }) => {
+    switch (keyCode) {
+        case 65:
+            console.log('left')
+            keys.left.pressed = true
+            break;
+
+        case 83:
+            console.log('down')
+            break
+
+        case 68:
+            console.log('right')
+            keys.right.pressed = true
+            break
+
+        case 87:
+            player.velocity.y -= 20
+            break
+    }
+})
+
+addEventListener('keyup' , ({ keyCode }) => {
+    switch (keyCode) {
+        case 65:
+            keys.left.pressed = false
+            break;
+
+        case 83:
+            break
+
+        case 68:
+            keys.right.pressed = false
+            break
+
+        case 87:
+            player.velocity.y -= 20
+            break
+    }
+})
